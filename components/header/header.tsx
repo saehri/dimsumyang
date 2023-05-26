6913d8cac8a8cc15f815d46b4c46003462b5ddb6
Saepul Bahri <bahreesaepul1@gmail.com>
2023-05-26 21:41:30 +0700

    added logo to the header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import {useState} from 'react';
+import Image from 'next/image';
 
 import MobileMenu from './mobile-menu';
 import MobileMenuToggleButton from '../buttons/mobile-menu-toggle-button';
@@ -22,6 +23,10 @@ export default function Header() {
         />
 
         <MobileMenu state={mobileHeaderVisible} />
+
+        <div className='w-[46px] h-[70px] sm:hidden absolute left-1/2 -translate-x-1/2'>
+          <Image src='/assets/decoration/logo.png' alt='0' fill={true} />
+        </div>
       </div>
     </div>
   );
