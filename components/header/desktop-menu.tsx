3bea3d1d300beada4691c4598a8c46b6b7226ba5
Saepul Bahri <bahreesaepul1@gmail.com>
2023-05-27 12:31:16 +0700

    lazy motion, desktop nav, and link enter duration
    1. Changed from using motion to "m" + LazyMotion for better animation performance
    2. Added desktop menu - not done yet
    3. Make the link animation enter duration shorter

---
+++ components/header/desktop-menu.tsx
@@ -0,0 +1,17 @@
+export default function DesktopMenu() {
+  return (
+    <div className='max-sm:hidden bg-yellow-400 max-w-7xl flex gap-4 justify-between'>
+      <div>
+        <div>Home</div>
+        <div>Menu</div>
+        <div>Prasmanan</div>
+      </div>
+
+      <div>
+        <div>Franchise</div>
+        <div>Reseller</div>
+        <div>Profile</div>
+      </div>
+    </div>
+  );
+}
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,31 +3,39 @@
 import React from 'react';
 import {useState} from 'react';
 import Image from 'next/image';
+import {LazyMotion, domAnimation} from 'framer-motion';
 
 import MobileMenu from './mobile-menu';
 import MobileMenuToggleButton from '../buttons/mobile-menu-toggle-button';
 import MobileBottomNavigation from './mobile-bottom-nav';
+import DesktopMenu from './desktop-menu';
 
 export default function Header() {
   const [mobileHeaderVisible, setMobileHeaderVisible] =
     useState<boolean>(false);
 
   return (
-    <div>
-      <div className='flex fixed w-full top-0 left-0 z-50 isolate items-end pt-10 px-8'>
-        <MobileBottomNavigation />
+    <LazyMotion features={domAnimation}>
+      <div>
+        <div className='flex fixed w-full top-0 left-0 z-50 isolate max-sm:items-end max-sm:pt-10 max-sm:px-8'>
+          <MobileBottomNavigation />
 
-        <MobileMenuToggleButton
-          state={mobileHeaderVisible}
-          setStatus={setMobileHeaderVisible}
-        />
+          <MobileMenuToggleButton
+            state={mobileHeaderVisible}
+            setStatus={setMobileHeaderVisible}
+          />
 
-        <MobileMenu state={mobileHeaderVisible} />
+          <MobileMenu state={mobileHeaderVisible} />
+          <DesktopMenu />
 
-        <div className='w-[46px] h-[70px] sm:hidden absolute left-1/2 -translate-x-1/2'>
-          <Image src='/assets/decoration/logo.png' alt='0' fill={true} />
+          <div className='w-[46px] h-[70px] sm:hidden absolute left-1/2 -translate-x-1/2'>
+            <Image src='/assets/decoration/logo.png' alt='0' fill={true} />
+          </div>
+          <div className='w-[46px] h-[70px] max-sm:hidden absolute left-1/2 -translate-x-1/2'>
+            <Image src='/assets/decoration/logo.png' alt='0' fill={true} />
+          </div>
         </div>
       </div>
-    </div>
+    </LazyMotion>
   );
 }
