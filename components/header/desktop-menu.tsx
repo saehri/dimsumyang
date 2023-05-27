b4489f190e01db37b819ad369cc3228710dd4f5e
Saepul Bahri <bahreesaepul1@gmail.com>
2023-05-27 20:40:55 +0700

    added animation

--- components/header/desktop-menu.tsx
+++ components/header/desktop-menu.tsx
@@ -1,17 +1,69 @@
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
+import {motion} from 'framer-motion';
+
+const menuStyle = 'flex gap-8 md:gap-10 lg:gap-12';
+
 export default function DesktopMenu() {
+  const pathname = usePathname();
+
   return (
-    <div className='max-sm:hidden bg-yellow-400 max-w-7xl flex gap-4 justify-between'>
-      <div>
-        <div>Home</div>
-        <div>Menu</div>
-        <div>Prasmanan</div>
+    <nav className='max-sm:hidden font-calistoga py-8 flex gap-4 justify-between w-full max-w-[867px] mx-auto px-8 lg:px-0'>
+      <div className={menuStyle}>
+        <NavLink path='/' currentPath={pathname}>
+          Home
+        </NavLink>
+
+        <NavLink path='/menu' currentPath={pathname}>
+          Menu
+        </NavLink>
+
+        <NavLink path='/prasmanan' currentPath={pathname}>
+          Prasmanan
+        </NavLink>
       </div>
 
-      <div>
-        <div>Franchise</div>
-        <div>Reseller</div>
-        <div>Profile</div>
+      <div className={menuStyle}>
+        <NavLink path='/franchise' currentPath={pathname}>
+          Franchise
+        </NavLink>
+
+        <NavLink path='/reseller' currentPath={pathname}>
+          Reseller
+        </NavLink>
+
+        <NavLink path='/profile' currentPath={pathname}>
+          Profile
+        </NavLink>
       </div>
-    </div>
+    </nav>
+  );
+}
+
+type NavLinkProps = {
+  children: React.ReactNode;
+  path: string;
+  currentPath: string;
+};
+
+function NavLink({children, path, currentPath}: NavLinkProps) {
+  return (
+    <Link href={path}>
+      <motion.div>
+        <span className='relative z-50 text-slate-950'>{children}</span>
+
+        {path === currentPath && (
+          <motion.div
+            layoutId='desktopNavLink'
+            className='absolute inset-0 z-10 rounded-full bg-yellow-500'
+            transition={{
+              type: 'tween',
+              duration: 0.7,
+              ease: [0.32, 0.72, 0, 1],
+            }}
+          ></motion.div>
+        )}
+      </motion.div>
+    </Link>
   );
 }
