d317e0e85fd067d3408a741b2f62f6343e5c7f7a
Saepul Bahri <bahreesaepul1@gmail.com>
2023-07-07 22:36:40 +0700

    header mobile logo

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
 
             <DesktopMenu />
 
-            {pathname === '/' && <MobileLogo />}
+            <MobileLogo />
           </div>
         </div>
       )}
--- components/header/mobile-logo.tsx
+++ components/header/mobile-logo.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import Image from 'next/image';
+import {usePathname} from 'next/navigation';
 import {useEffect} from 'react';
 import {m, useMotionValue, useScroll} from 'framer-motion';
 
 const logoStyle =
   'w-[46px] h-[70px] lg:top-4 lg:hidden fixed top-2 left-1/2 -translate-x-1/2';
 export default function MobileLogo() {
+  const pathname = usePathname();
+
   const logoYellowOpacity = useMotionValue(0);
   const logoWhiteOpacity = useMotionValue(1);
   const {scrollY} = useScroll();
@@ -25,26 +28,44 @@ export default function MobileLogo() {
 
   return (
     <>
-      <m.div
-        style={{opacity: logoWhiteOpacity}}
-        transition={{duration: 1}}
-        className={logoStyle}
-      >
-        <Image
-          src='/assets/decoration/logo white.png'
-          alt='0'
-          fill={true}
-          priority
-        />
-      </m.div>
+      {pathname === '/' ? (
+        <>
+          <m.div
+            style={{opacity: logoWhiteOpacity}}
+            transition={{duration: 1}}
+            className={logoStyle}
+          >
+            <Image
+              src='/assets/decoration/logo white.png'
+              alt='0'
+              fill={true}
+              priority
+            />
+          </m.div>
 
-      <m.div
-        style={{opacity: logoYellowOpacity}}
-        transition={{duration: 1}}
-        className={logoStyle}
-      >
-        <Image src='/assets/decoration/logo.png' alt='0' fill={true} priority />
-      </m.div>
+          <m.div
+            style={{opacity: logoYellowOpacity}}
+            transition={{duration: 1}}
+            className={logoStyle}
+          >
+            <Image
+              src='/assets/decoration/logo.png'
+              alt='0'
+              fill={true}
+              priority
+            />
+          </m.div>
+        </>
+      ) : (
+        <div className={logoStyle}>
+          <Image
+            src='/assets/decoration/logo.png'
+            alt='0'
+            fill={true}
+            priority
+          />
+        </div>
+      )}
     </>
   );
 }
