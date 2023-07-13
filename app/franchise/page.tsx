1bfdd4f65fa8af824c79622331e04b40a630588c
Saepul Bahri <bahreesaepul1@gmail.com>
2023-07-13 11:31:38 +0700

    try to fixed the franchise page error

--- app/franchise/page.tsx
+++ app/franchise/page.tsx
@@ -1,10 +1,14 @@
 'use client';
+
 import {useRouter} from 'next/navigation';
 import {useEffect} from 'react';
 
 export default function Redirect() {
   const router = useRouter();
-  router.push('/franchise/dim');
 
-  return null;
+  useEffect(() => {
+    router.push('/franchise/dim');
+  }, []);
+
+  return <div></div>;
 }
