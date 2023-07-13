2956a0b400128bcae7adf31bc64c84f0482b20a1
Saepul Bahri <bahreesaepul1@gmail.com>
2023-07-13 11:23:57 +0700

    changed tagline font sizes

--- components/small/tagline.tsx
+++ components/small/tagline.tsx
@@ -6,12 +6,12 @@ type Props = {
 export default function PageTagline({main, secondary}: Props) {
   return (
     <>
-      <h1 className='text-xl font-calistoga text-primary-orange text-center md:text-4xl mb-2 lg:mb-3'>
+      <h1 className='text-xl font-calistoga text-primary-orange text-center md:text-5xl mb-2 lg:mb-3'>
         {main}
       </h1>
 
       {secondary && (
-        <h2 className='font-outfit text-center mx-auto text-slate-950 text-sm md:text-base mb-10 lg:mb-20 px-4'>
+        <h2 className='font-outfit text-center mx-auto text-slate-950 text-sm md:text-2xl mb-10 lg:mb-20 px-4'>
           {secondary}
         </h2>
       )}
