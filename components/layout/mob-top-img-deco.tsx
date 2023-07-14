32a05e7a237c1720d9c76720aa58933f464329f2
Saepul Bahri <bahreesaepul1@gmail.com>
2023-07-14 09:04:26 +0700

    idk

--- components/layout/mob-top-img-deco.tsx
+++ components/layout/mob-top-img-deco.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 
-export default function MobileTopImageDecoration() {
+export default function MobileTopImageDecoration({
+  type = 'menu',
+}: {
+  type?: string;
+}) {
   return (
     <div
-      className='absolute top-0 left-0 w-full overflow-hidden pt-[calc((27/43)*100%)] lg:pt-[calc((541/1449)*100%)]'
+      className={`absolute top-0 left-0 w-full overflow-hidden ${
+        type !== 'menu'
+          ? 'pt-[calc((41/43)*100%)]'
+          : 'pt-[calc((27/43)*100%)] lg:pt-[calc((541/1449)*100%)]'
+      }`}
       aria-hidden='true'
     >
       <div className='absolute z-0 top-0 left-0 w-full h-full'>
         <Image
           className='object-cover lg:hidden'
-          src='/assets/decoration/mobile menu page top bg.png'
+          src={`/assets/decoration/mobile ${type} page top bg.png`}
           fill={true}
           sizes='(max-width: 1024px) 1024px'
           alt=''
@@ -18,7 +26,7 @@ export default function MobileTopImageDecoration() {
 
         <Image
           className='object-cover hidden lg:block'
-          src='/assets/decoration/desktop menu page top bg.png'
+          src={`/assets/decoration/desktop ${type} page top bg.png`}
           fill={true}
           sizes='(min-width: 1024px) 2889px'
           alt=''
